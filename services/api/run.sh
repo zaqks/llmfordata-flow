7d4dc2bec911c4f8d0b52df6b4d492050425bb4f
zaqks <imzakbro@gmail.com>
2025-12-26 20:32:00 +0100

    admin pwd

--- services/api/run.sh
+++ services/api/run.sh
@@ -3,5 +3,9 @@
 # run airflow
 airflow standalone &
 
+# change pwd
+while [ ! -f /app/airflow/simple_auth_manager_passwords.json.generated ]; do sleep 1; done; echo '{"admin":"admin"}' > /app/airflow/simple_auth_manager_passwords.json.generated
+
+
 # run dj
 uvicorn _main_app.asgi:application --host 0.0.0.0 --port 8000 --reload --app-dir ./dj
