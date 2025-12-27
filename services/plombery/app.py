95ea921f73f4f0a0dcf8bdc37ff5538e375bc466
zaqks <imzakbro@gmail.com>
2025-12-27 16:07:00 +0100

    db env

--- services/plombery/app.py
+++ services/plombery/app.py
@@ -3,4 +3,6 @@ import uvicorn
 
 
 if __name__ == "__main__":
-    uvicorn.run("plombery:get_app", reload=True, factory=True, port=8000, host="0.0.0.0")
+    uvicorn.run(
+        "plombery:get_app", reload=True, factory=True, port=8000, host="0.0.0.0"
+    )
