f281552ed1c35cc302a3acf0a3f09a62539ae161
zaqks <imzakbro@gmail.com>
2025-12-28 19:07:25 +0100

    db models

--- services/plombery/test.py
+++
@@ -1,7 +0,0 @@
-from src.flows.gen.report_llm import *
-from src.flows.gen.charts_generation import *
-from src.flows.gen.report_concat import *
-
-main()
-generate_all_charts()
-append_charts_section("report.md")
