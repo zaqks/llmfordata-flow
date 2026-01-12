295caffd1f5662b3e2ee1c6a1a5b1dd84670486f
zaqks <imzakbro@gmail.com>
2026-01-12 13:20:01 +0100

    enable all flows

--- services/plombery/app.py
+++ services/plombery/app.py
@@ -5,6 +5,11 @@ from src.flows.ingestion.ingestion_arxiv import *
 from src.flows.ingestion.ingestion_huggingface import *
 from src.flows.ingestion.ingestion_nvidia import *
 
+from src.flows.ingestion.ingestion_databricks import *
+from src.flows.ingestion.ingestion_paperswithcode import *
+from src.flows.ingestion.ingestion_semanticscholar import *
+from src.flows.ingestion.ingestion_snowflake import *
+
 from src.flows.analysis.analysis_llm import *
 from src.flows.gen.report_generation import *
 
